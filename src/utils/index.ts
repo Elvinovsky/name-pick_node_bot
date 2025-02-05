960b528836297fdd2303448ed157c9e84b824f1b
el <shipagakerimov@mail.ru>
2025-02-05 17:28:43 +0500

    refactoring

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export * from './queryCondition';
 export * from './btn';
+export * from './ruToEnLayout';
+export * from './uniqueNested';
--- src/utils/queryCondition.ts
+++ src/utils/queryCondition.ts
@@ -1,21 +1,20 @@
 import { $Enums, Prisma } from '@prisma/client';
-import { btnKeysByVal } from './btn';
-import { ButtonsLayout } from '../core/TGBot/buttons';
+import { Logger } from '../core';
 
 export function queryCondition(filters?: string[] | string): Prisma.NameWhereInput {
+  Logger.shared.dbg('queryCondition', filters);
   if (!filters?.length) return {};
-  if (!Array.isArray(filters)) return {};
+  Array.isArray(filters) ? filters : (filters = [filters]);
 
   const genderFilters = filters.filter((f) => Object.keys($Enums.Gender).includes(f)) as $Enums.Gender[];
   const originFilters = filters.filter((f) => Object.keys($Enums.Origin).includes(f)) as $Enums.Origin[];
-  const firstCategory = filters.includes('rare') ? 'rare' : undefined;
+  const rareFilter = filters.includes('rare') ? 'rare' : undefined;
 
   const originCondition = originFilters.length > 0 ? [{ OR: originFilters.map((f) => ({ origin: f })) }] : [];
   const genderCondition = genderFilters.length === 1 ? [{ gender: genderFilters[0] }] : [];
-  const categoryCondition = firstCategory ? [{ category: firstCategory }] : [];
-  const notRareCondition = firstCategory ? [] : [{ category: { not: 'rare' } }];
+  const categoryCondition = rareFilter ? [{ category: { in: ['rare', 'rareUnusual', 'classicOld'] } }] : [{ category: { notIn: ['rare', 'rareUnusual', 'classicOld'] } }];
 
   return {
-    AND: [...originCondition, ...genderCondition, ...categoryCondition, ...notRareCondition],
+    AND: [...originCondition, ...genderCondition, ...categoryCondition],
   };
 }
